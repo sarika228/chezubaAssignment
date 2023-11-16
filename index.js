0abaa016dd2c8e71e726859ebd493d8d7926fd1d
sarika228 <81947787+sarika228@users.noreply.github.com>
2023-11-16 14:34:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(bodyParser.json());
+
+
+const orders = []; 
+
+app.get('/api/order/:orderid', (req, res) => {
+  const order = orders.find((o) => o.id === parseInt(req.params.orderid));
+  if (!order) {
+    res.status(404).json({ error: 'Order not found' });
+  } else {
+    res.json(order);
+  }
+});
+
+app.post('/api/order', (req, res) => {
+  const newOrder = {
+    id: orders.length + 1,
+    itemType: req.body.itemType,
+    orderState: req.body.orderState,
+  };
+  orders.push(newOrder);
+  res.json({ orderId: newOrder.id });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
